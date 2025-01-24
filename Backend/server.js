ebb78210f6854e7d748e91ce6cc2c3698f08a241
malhaouit <abdelmounaim.abdel@gmail.com>
2025-01-24 22:50:51 +0000

    Add cors and call product routes

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
+const productRoutes = require('./src/routes/productRoutes');
 const connectDB = require('./src/config/db');
 
 const app = express();
@@ -8,6 +10,9 @@ const PORT = process.env.PORT || 3000;
 // Middleware to parse JSON
 app.use(express.json());
 
+// CORS
+app.use(cors());
+
 // Connect to database
 connectDB();
 
@@ -15,6 +20,8 @@ connectDB();
 app.get('/', (req, res) => {
   res.send('Welcome to Electronics Inventory Management!');
 });
+
+app.use('/api', productRoutes);
 
 // Start the server
 app.listen(PORT, () => {
