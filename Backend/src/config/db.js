f43d0dfa6a3ef8fccf450ebe3ea9e29823e31de5
malhaouit <abdelmounaim.abdel@gmail.com>
2025-01-24 22:51:26 +0000

    Create database connection

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.DB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     });
     console.log('MongoDB connected!');
   } catch (error) {
